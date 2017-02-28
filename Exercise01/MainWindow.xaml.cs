9deb02d555a12e7e5426293affe77917882b9dde
alex855k <alexander2341@gmail.com>
2017-02-28 09:38:17 +0100

    2nd Exercise is just a question u need to answer in text.

---
+++ Exercise01/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Exercise01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page2();
+        }
+    }
+}
---
+++ Exercise3/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Exercise3.ViewModel;
+using System.Windows;
+
+namespace Exercise3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BlueView_Clicked(object sender, RoutedEventArgs args)
+        {
+            DataContext = new BlueViewModel();
+        }
+        private void RedView_Clicked(object sender, RoutedEventArgs args)
+        {
+            DataContext = new RedViewModel();
+        }
+
+        private void OrangeView_Clicked(object sender, RoutedEventArgs args)
+        {
+            DataContext = new OrangeViewModel();
+        }
+    }
+}
